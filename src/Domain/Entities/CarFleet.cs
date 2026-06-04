c3f8b32dc0907748728cbc9e15a0ed0fa3fbef20
Jakub Wojtyna <jakub.wojtyna06@gmail.com>
2026-06-04 13:36:42 +0200

    Add factory for car fleet

--- src/Domain/Entities/CarFleet.cs
+++ src/Domain/Entities/CarFleet.cs
@@ -4,8 +4,23 @@ namespace Domain.Entities;
 
 public class CarFleet : BaseEntity
 {
-    public Guid TeamId { get; set; }
-    public Team Team { get; set; }
+    public Guid TeamId { get; private set; }
+    public Team Team { get; private set; }
 
     public List<Car> Cars { get; set; } = new();
+    
+    protected CarFleet(){}
+
+    private CarFleet(Guid teamId)
+    {
+        TeamId = teamId;
+    }
+
+    public static CarFleet Create(Guid teamId)
+    {
+        if (teamId == Guid.Empty)
+            throw new ArgumentException("Team id cannot be empty");
+        
+        return new CarFleet(teamId);
+    }
 }
